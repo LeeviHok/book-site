455f8078ffaf94df694d5ece656317f92b7d33b1
Leevi Hokkanen <leevi.sahkoposti@gmail.com>
2023-02-22 20:21:34 +0200

    Added prefix to ORM model constants.
    ORM model constants are now prefixed with model names. This keeps
    constant names unique if new models with new constants are added.

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,16 +2,16 @@ from sqlalchemy import Column, Integer, String, Text
 
 from .database import Base
 
-AUTHOR_MAX_LEN = 500
-TITLE_MAX_LEN = 500
+BOOK_AUTHOR_MAX_LEN = 500
+BOOK_TITLE_MAX_LEN = 500
 
 
 class Book(Base):
     __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True, index=True)
-    author = Column(String(length=AUTHOR_MAX_LEN), nullable=False)
-    title = Column(String(length=TITLE_MAX_LEN), nullable=False)
+    author = Column(String(length=BOOK_AUTHOR_MAX_LEN), nullable=False)
+    title = Column(String(length=BOOK_TITLE_MAX_LEN), nullable=False)
     description = Column(Text, nullable=False)
 
     # Printable representation of the book object
